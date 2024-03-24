c048382cefc98558cc1f42a147a0b709cd1fc603
Heath S <heath.sage.2003@gmail.com>
2024-03-24 23:05:23 +0000

    [FIX] Using react routes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,15 @@
 // src/pages/Home.js
 import React from 'react';
 import './pages.css'; // Import the CSS file here
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
         <div className="container">
             <h1 className="home-title">Welcome to Heath Sage's Portfolio</h1>
-            {/* Add CTA button*/}
-            <a href="/projects" className="projects-cta">View Projects</a>
+            {/* Add CTA button and route to projects page */}
+            <Link to="/projects" className="projects-cta">View Projects</Link>
+
         </div>
 
     );
